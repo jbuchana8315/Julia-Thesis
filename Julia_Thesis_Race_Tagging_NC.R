4f8396d44ab69e878c867c079b6cb56afe6e71ab
jbuchana8315 <jbuchana8315@scrippscollege.edu>
2025-11-29 14:25:18 -0800

    Add files via upload

---
+++ Julia_Thesis_Race_Tagging_NC.R
@@ -0,0 +1,80 @@
+#Julia Buchanan Thesis
+#North Carolina voter race tagging assessment 
+
+rm(list = ls())
+library(wru)
+library(tidyverse)
+
+##########################################
+##    this code demonstrates how to race tag 
+##    a *new* voter file using the wru package. 
+##
+##    race scores have already been pre-
+##    computed for the 2020 North Carolina voter file 
+##     and can be found in the folder
+##    "Processed Data/Voter Files/Non-LA Files" 
+##########################################
+
+##########################################
+##             data loading             ##
+##########################################
+
+# set the working directory
+setwd("E:/Users/Processed Data/")
+
+# load the 2020 NC voter file
+load("Voter Files/Non-LA Files/nc_voterfile_2020.rData") 
+
+# load the 2010 NC Census data
+# use the get_census_data function in wru to save this file
+# to the remote machine 
+load("Census Data/ncCensus_2010.rData")
+
+###########################################
+##           race tagging code           ##
+###########################################
+
+# add state and county to the file
+vf$state <- 'NC'
+
+# rename columns to match wru names
+names(vf)[names(vf) == "Residence_Addresses_CensusBlock"] <- 'block'
+names(vf)[names(vf) == "Residence_Addresses_CensusTract"] <- 'tract'
+names(vf)[names(vf) == "County"] <- 'county'
+names(vf)[names(vf) == "Voters_LastName"] <- 'surname'
+names(vf)[names(vf) == "Voters_FirstName"] <- 'first'
+names(vf)[names(vf) == "Voters_MiddleName"] <- 'middle'
+
+# run bisg at the tract level 
+vf <- predict_race(vf, names.to.use = 'surname, first, middle', skip_bad_geos = TRUE, 
+                       census.geo = 'tract', census.data = ncCensus_2010, year = "2010")
+
+##########################################
+##           simple analytics           ##
+##########################################
+
+# what are the voter file racial distributions?
+vfRacialDistributions <- vf %>%
+  summarise(white = mean(pred.whi, na.rm = TRUE), 
+            black = mean(pred.bla, na.rm = TRUE),
+            hispanic = mean(pred.his, na.rm = TRUE), 
+            aapi = mean(pred.asi, na.rm = TRUE), 
+            other = mean(pred.oth, na.rm = TRUE))
+
+# what are the 2022 general electorate racial distributions?
+g2022RacialDistributions <- laData %>%
+  filter(!is.na(General_2022_11_08)) %>%
+  summarise(white = mean(pred.whi, na.rm = TRUE), 
+            black = mean(pred.bla, na.rm = TRUE),
+            hispanic = mean(pred.his, na.rm = TRUE), 
+            aapi = mean(pred.asi, na.rm = TRUE), 
+            other = mean(pred.oth, na.rm = TRUE))
+
+# what are the 2022 primary electorate racial distributions?
+p2022RacialDistributions <- laData %>%
+  filter(!is.na(Primary_2022_06_07)) %>%
+  summarise(white = mean(pred.whi, na.rm = TRUE), 
+            black = mean(pred.bla, na.rm = TRUE),
+            hispanic = mean(pred.his, na.rm = TRUE), 
+            aapi = mean(pred.asi, na.rm = TRUE), 
+            other = mean(pred.oth, na.rm = TRUE))
