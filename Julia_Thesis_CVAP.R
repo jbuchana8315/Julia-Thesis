0258c1d3d87fe709894edc60e7891e710875d110
jbuchana8315 <jbuchana8315@scrippscollege.edu>
2026-03-10 17:59:46 -0700

    fixed
    Updated the race tagging code and added predictive accuracy calculations for the voter file. Enhanced calibration plotting functions for better visualization of results.

--- Julia_Thesis_CVAP.R
+++ Julia_Thesis_CVAP.R
@@ -89,6 +89,7 @@ vfRacialDistributions
 
 #predictive accuracy against MAP predictions
 taggedvf <- taggedvf %>%
+filter(!is.na(CountyEthnic.Description)) %>%
   mutate(
     ethnic_desc = toupper(CountyEthnic.Description),     
     
@@ -258,3 +259,4 @@ cal_other <- calibration_table(taggedvf, "pred.oth", "OTHER")
 cat("ECE (Other):", round(attr(cal_other, "ece"), 6), "\n")
 p_other <- plot_calibration(cal_other, "Other")
 print(p_other)
+
